2d1c4dbaedc44c46a3260764763723f3e4ffac92
alexanderng <alex@sitecore.net>
2023-12-31 15:36:38 -0500

    disable code for async redirects in next.config.js

--- next.config.js
+++ next.config.js
@@ -111,11 +111,13 @@ const nextConfig = {
         destination: `${jssConfig.sitecoreApiHost}/sitecore/service/:path*`,
       },
     ];
-  },
+  }
+  /* alextest
   async redirects() {
     //Redirects have been split and moved into plugins in /lib/next-config/plugins
     return [];
   },
+  */
 };
 
 module.exports = () => {
