137e18645df44e092c2f6f6ede90a0f0a4219b7a
alexanderng <alex@sitecore.net>
2024-01-05 18:52:53 -0500

    remove components affected by disabling prettier

--- src/components/EmbeddedComponent/EmbeddedComponent.tsx
+++
@@ -1,7 +0,0 @@
-import styles from './EmbeddedComponent.module.css';
-
-const EmbeddedComponent = (): JSX.Element => {
-  return <div className={styles['embedded-component']}>EmbeddedComponent</div>;
-};
-
-export default EmbeddedComponent;
